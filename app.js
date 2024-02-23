3e5e44fc63a3afcc9f96910355f48a6a8c992f84
bharath9812 <[mandadhibharathsimham@gmail.com])>
2024-02-23 23:37:21 +0530

    Created Model, express test route

---
+++ app.js
@@ -0,0 +1,44 @@
+// npm i express mongoose ejs
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const mongoose = require('mongoose');
+
+//getting the model
+const CampGround = require('./models/campground')
+
+main()
+
+async function main() {
+    await mongoose.connect('mongodb://127.0.0.1:27017/yelp')
+        .then(() => {
+            console.log('MONGO connection open') 
+        })
+        .catch(err => {
+            console.log('MONGO connection error', err);
+    })
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+} 
+
+
+// ====================================================================================================
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('home')
+})
+
+app.get('/makecampground', async (req, res) => {
+    const camp = new CampGround({ title: 'Backyad',description:'cheap'})
+    await camp.save();
+    res.send('camo');
+})
+
+app.listen(3000, () => {
+    console.log('Working on port 3K');
+})
