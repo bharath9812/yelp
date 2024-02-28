44b44c3d7125f8249f9d213c178455569b8c7dbd
bharath9812 <mandadhibharathsimham@gmail.com>
2024-02-28 23:47:39 +0530

    joi validation schema

---
+++ schemas.js
@@ -0,0 +1,13 @@
+const Joi = require('joi'); 
+// Joi object validation schema 
+const campgroundSchema = Joi.object({
+    campground: Joi.object({
+        title: Joi.string().required(),
+        price: Joi.number().required().min(0),
+        image: Joi.string().required(),
+        location: Joi.string().required(),
+        description: Joi.string().required()
+    }).required()
+})
+
+module.exports.campgroundSchema = campgroundSchema;
