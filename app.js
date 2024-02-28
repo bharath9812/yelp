5868cf4265471f188517d2517383df6cc8f324aa
bharath9812 <mandadhibharathsimham@gmail.com>
2024-02-28 23:37:24 +0530

    before error validation

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
-// npm i express mongoose ejs method-override ejs-mate // npm packages used (install these first)
+// npm i express mongoose ejs method-override ejs-mate joi
 
-
+//requiring packages needed
 const express = require('express');
 const ejsMate = require('ejs-mate');
-
 const app = express();
 // requiring path to set absolute path for the /views
 const path = require('path');
-
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const Joi = require('joi');
+const { campgroundSchema } = require('./schemas.js')
 
 //getting the model
-const CampGround = require('./models/campground')
+const CampGround = require('./models/campground');
+const catchAsync = require('./utils/catchAsync');
+const ExpressError = require('./utils/ExpressError');
 
 
-
-
+// ====================================================================================================
+// connecting mongoose
 
 main()
 
@@ -45,15 +47,31 @@ app.engine('ejs',ejsMate,);
 // ===============================================================================================================
 
 
+const validateCampground = (req, res, next) => {
+    
+    const {error} = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(el => el.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    else {
+        next();
+    }
+    // console.log(result);
+}
+
+
+//      RESTFUL ROUTES
+
 app.get('/', (req, res) => {
     res.render('home')
 })
 
 // index route
-app.get('/campgrounds', async (req, res) => {
+app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await CampGround.find({});
     res.render('campgrounds/index', {campgrounds});
-})
+}))
 
 
 // route to create new campgrounds
@@ -64,49 +82,62 @@ app.get('/campgrounds/new', (req, res) => {
 
 // new campground form end point to post req
 
-app.post('/campgrounds', async (req, res) => {
+app.post('/campgrounds', validateCampground, catchAsync(async (req, res,next) => {
+    // if (!req.body.campground) throw new ExpressError('Invalid Campground Data',400);
     const campground = new CampGround(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
     // res.send(req.body.campground);
 
-} )
+}) )
 
 
 // show route
-app.get('/campgrounds/:id', async (req, res) => {
+app.get('/campgrounds/:id', catchAsync(async (req, res) => {
     const id = req.params.id;
     const campground = await CampGround.findById(id);
     res.render('campgrounds/show', {campground})
-})
+}))
 
 // route to edit the campground 
-app.get('/campgrounds/:id/edit', async (req, res) => {
+app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const id = req.params.id;
     const campground = await CampGround.findById(id);
     res.render('campgrounds/edit.ejs', {campground})
-})
+}))
 
 // put request to submit the edit form
 
-app.put('/campgrounds/:id', async (req, res) => {
+app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await CampGround.findByIdAndUpdate(id, { ...req.body.campground });
     // res.send('updated');
     res.redirect(`/campgrounds/${campground._id}`)
-})
+}))
 
 // route to delete the campground
 
-app.delete('/campgrounds/:id', async (req, res) => {
+app.delete('/campgrounds/:id', catchAsync(async (req, res) => {
     const { id } = req.params;
     await CampGround.findByIdAndDelete(id);
     res.redirect('/campgrounds');
 
+}))
+
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500 } = err;
+    if (!err.message) err.message = 'Something Went Wrong!'
+    res.status(statusCode).render('error', { err })
 })
+
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404))
+    // res.send("Not found");
+});
+
+
 
 app.listen(3000, () => {
     console.log('Working on port 3K');
 })
-
-
