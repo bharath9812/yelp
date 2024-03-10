72fdeaa9436fa6510ebecb590031d690860bb2a4
bharath9812 <mandadhibharathsimham@gmail.com>
2024-03-11 00:43:11 +0530

    authorization with reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const express = require('express');
 //by default express router like to keep params separate
 //(id can't be accessed from ' / campgrounds /: id / reviews'), so we merge the params
 const router = express.Router({ mergeParams: true});
-const { validateReview, isLoggedIn } = require('../middleware.js');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware.js');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/ExpressError');
 
@@ -27,13 +27,12 @@ router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
 }))
 
 // findByIdAndDelete on triggers the findOneAndDelete middleware defined in campground.js
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId', isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await CampGround.findByIdAndUpdate(id, { $pull: { reviews: reviewId } }); // pull used like a index to get from an array
     await Review.findByIdAndDelete(reviewId);
     req.flash('success', 'Successfully deleted review')
     res.redirect(`/campgrounds/${id}`);
-    // res.send('deleted man bye')
 }))
 
 module.exports = router;
