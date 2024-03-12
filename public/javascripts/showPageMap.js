83cb6aa26d95cfeb74bfa7361ed7983f2b8ddfd3
bharath9812 <mandadhibharathsimham@gmail.com>
2024-03-12 21:19:30 +0530

    map popup and style

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -1,7 +1,8 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    // style: 'mapbox://styles/mapbox/light-v10', // stylesheet location
+    // style: 'mapbox://styles/mapbox/satellite-streets-v12',
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8 // starting zoom
 });
