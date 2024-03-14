93970c4e257603bfdeb0f84f0a48911f2a36ec2e
bharath9812 <mandadhibharathsimham@gmail.com>
2024-03-14 10:45:31 +0530

    adding map controls

--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -2,10 +2,12 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     // style: 'mapbox://styles/mapbox/light-v10', // stylesheet location (plain light-varaint)
-    style: 'mapbox://styles/mapbox/satellite-streets-v12', // satelite views
+    // style: 'mapbox://styles/mapbox/satellite-streets-v12', // satelite views
+    style: 'mapbox://styles/mapbox/dark-v10',
     center: campground.geometry.coordinates, // starting position [lng, lat]
     zoom: 8 // starting zoom
 });
+map.addControl(new mapboxgl.NavigationControl());
 
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
